a79d76a454529f66d514e309933974defa2c5860
jacopo <jack1395@hotmail.it>
2016-03-29 17:03:33 +0200

    Update log.c
    formattazione del testo nel log.txt

--- log.c
+++ log.c
@@ -4,6 +4,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h> //write()
 #include <string.h>//warning strlen strcat
 #include <fcntl.h>// serve per le opzioni di apertura del file
@@ -19,11 +20,9 @@ void logMsg(char* toWrite) {
 
     int ret;
     
-    if ((file = open(NOME_FILE, O_WRONLY | O_CREAT | O_APPEND, 0666)) == -1) {
-        fprintf(stderr, "Errore apertura file di log\n");
-        exit(EXIT_FAILURE);
-    }    
-
+    file = open(NOME_FILE, O_WRONLY | O_CREAT | O_APPEND, 0666); 
+	ERROR_HELPER(file,"Errore apertura file log.txt");
+	
     //	getting current time and date
     time_t mytime;
     mytime = time(NULL);
@@ -31,9 +30,12 @@ void logMsg(char* toWrite) {
     //	gestione segnali
     //sigset_t maschera;
 	char* date=ctime(&mytime);
-	
+	char msg[strlen(toWrite)+3] ; 
+	strcat(msg,toWrite);
+	strcat(msg,"\n\n"); //aggiunte 2 \n per rendere il file di log più leggibile
+    
     ret = write(file, date, strlen(date));
-    ret |= write(file, toWrite, strlen(toWrite));
+    ret |= write(file, msg, strlen(msg));
     ERROR_HELPER(ret, "Errore scrittura operazione server su file");
     
     ret=close(file);
