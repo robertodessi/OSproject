c10ea381676f0a80fca6ba8511e4bbe357fd1a22
jacopo <jack1395@hotmail.it>
2016-05-15 14:56:41 +0200

    vero initial commit per windows (non so perchè ma su github c'è un altro codice)

--- client_windows/client_windows/client_windows.cpp
+++ client_windows/client_windows/client_windows.cpp
@@ -1,54 +1,194 @@
 // client_windows.cpp : definisce il punto di ingresso dell'applicazione console.
 //
 
-#include "stdafx.h"
+/*
+
+TCPCLIENT.C
+==========
+
+*/
 
 
-
-#include "stdafx.h"
-
-#include <windows.h>
 #include <stdio.h>
+#include <winsock.h>
 #include <stdlib.h>
 
-#define ECHOING_EXIT_CODE 2
+///#include "helper.h"           /*  Our own helper functions  */
 
-int main(int argc, char *argv[]) {
+/*  Global constants  */
 
-	BOOL newprocess;
-	STARTUPINFO si;
-	PROCESS_INFORMATION  pi;
-	DWORD exitcode;
-	char buff[128];
+#define MAX_LINE           (1000)
+#define MAX_CHAR           (1000)
 
-	memset(&si, 0, sizeof(si));
-	memset(&pi, 0, sizeof(pi));
-	si.cb = sizeof(si);
+int ParseCmdLine(int argc, char *argv[], char **szAddress, char **szPort);
 
-	if (argc == 2) {
-		printf("Starting up echoing process ....\n");
-		fflush(stdout);
-		newprocess = CreateProcessA((LPCSTR)argv[1], (LPSTR)argv[1], NULL, NULL, FALSE, NORMAL_PRIORITY_CLASS, NULL, NULL, (LPSTARTUPINFO)&si, &pi);
-		if (newprocess == FALSE) {
-			printf(" failure\n\n");
-			ExitProcess(-1);
-		};
-		printf(" done\n\n");
-		WaitForSingleObject(pi.hProcess, INFINITE);
-		GetExitCodeProcess(pi.hProcess, &exitcode);
-		printf("\nEchoing process exited with code %d\n", exitcode);
-		fflush(stdout);
-
-	}
-	else {
-		printf("\tHere I'm for echoing\n\t");
-		fflush(stdout);
-		while (1) {
-			scanf("%s", buff);
-			if (strcmp(buff, "bye") == 0) ExitProcess(ECHOING_EXIT_CODE);
-			printf("\t%s\n\t", buff);
-			fflush(stdout);
+void* Ricevi(int socket_desc){
+	unsigned int sum = 0;
+	char msg_recv[1024];
+	size_t msg_recv_len = sizeof(msg_recv);
+	while (1) {
+		int recv_bytes = 0;
+		while ((recv_bytes = recv(socket_desc, msg_recv, msg_recv_len, 0)) < 0) {
+			if (errno == EINTR) continue;
+			exit(-1);
 		}
+		if (recv_bytes > 0)printf("ricevuto: %s\n", msg_recv);
+		sum += recv_bytes;
 	}
+	return (void*)sum;
 }
 
+void* Invia(int socket_desc) {	
+	int ret;
+	char buf[1024];
+
+	unsigned int sum = 0;
+	while (1) {
+		printf("send: ");
+		fgets(buf, 1024, stdin); //fgets prende anche il carattere invio
+		size_t buf_len = strlen(buf);
+		--buf_len; // remove '\n' from the end of the message
+
+		while ((ret = send(socket_desc, buf, buf_len, 0)) < 0) {
+			if (errno == EINTR) continue;
+			exit(-1);
+		}
+		sum += ret;
+	}
+	return (void*)sum;
+}
+
+/*  main()  */
+
+int main(int argc, char *argv[])
+{
+	SOCKET    socket_desc;           /*  connection socket         */
+	long port;                  /*  port number               */
+	struct    sockaddr_in servaddr;  /*  socket address structure  */
+	char     *szAddress;             /*  Holds remote IP address   */
+	char     *szPort;                /*  Holds remote port         */
+	char     *endptr;                /*  for strtol()              */
+	struct	  hostent *he;
+
+	HANDLE thread_recv;
+	HANDLE thread_send;
+	DWORD id_recv;
+	DWORD id_send;
+
+	u_long    nRemoteAddr;
+	WSADATA   wsaData;
+
+	he = NULL;
+
+	/*  Get command line arguments  */
+
+	ParseCmdLine(argc, argv, &szAddress, &szPort);
+
+
+	/*  Set the remote port  */
+
+	port = strtol(szPort, &endptr, 0);
+	if (*endptr){
+		printf("client: porta non riconosciuta.\n");
+		exit(EXIT_FAILURE);
+	}
+
+
+	if (WSAStartup(MAKEWORD(1, 1), &wsaData) != 0){
+		printf("errore in WSAStartup()\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/*  Create the listening socket  */
+
+	if ((socket_desc = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET){
+		fprintf(stderr, "client: errore durante la creazione della socket.\n");
+		exit(EXIT_FAILURE);
+	}
+
+
+	/*  Set all bytes in socket address structure to
+	zero, and fill in the relevant data members   */
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(port);
+
+	/*  Set the remote IP address  */
+	nRemoteAddr = inet_addr(szAddress);
+	if (nRemoteAddr == INADDR_NONE){
+		printf("client: indirizzo IP non valido.\nclient: risoluzione nome...");
+		if ((he = gethostbyname(szAddress)) == 0){
+			printf("fallita.\n");
+			exit(EXIT_FAILURE);
+		}
+		printf("riuscita.\n\n");
+		nRemoteAddr = *((u_long *)he->h_addr_list[0]);
+	}
+	servaddr.sin_addr.s_addr = nRemoteAddr;
+
+
+	/*  connect() to the remote echo server  */
+	if (connect(socket_desc, (struct sockaddr *) &servaddr, sizeof(servaddr)) == SOCKET_ERROR){
+		printf("client: errore durante la connect.\n");
+		exit(EXIT_FAILURE);
+	}
+
+
+	thread_recv = CreateThread(NULL,
+		0,
+		(LPTHREAD_START_ROUTINE)Ricevi,
+		(LPVOID)socket_desc,
+		NORMAL_PRIORITY_CLASS,
+		&id_recv);
+
+	if (thread_recv == NULL) {
+		printf("cannot create thread %d\n",id_recv);
+		ExitProcess(-1);
+	}
+
+	thread_send = CreateThread(NULL,
+		0,
+		(LPTHREAD_START_ROUTINE)Invia,
+		(LPVOID)socket_desc,
+		NORMAL_PRIORITY_CLASS,
+		&id_send);
+
+	if (thread_send == NULL) {
+		printf("cannot create thread %d\n",id_send);
+		ExitProcess(-1);
+	}
+
+	WaitForSingleObject(thread_recv, INFINITE);
+	WaitForSingleObject(thread_send, INFINITE);
+
+	WSACleanup();
+	return EXIT_SUCCESS;
+}
+
+
+int ParseCmdLine(int argc, char *argv[], char **szAddress, char **szPort){
+	int n = 1;
+
+	while (n < argc){
+		if (!strncmp(argv[n], "-a", 2) || !strncmp(argv[n], "-A", 2)){
+			*szAddress = argv[++n];
+		}
+		else
+			if (!strncmp(argv[n], "-p", 2) || !strncmp(argv[n], "-P", 2)){
+				*szPort = argv[++n];
+			}
+			else
+				if (!strncmp(argv[n], "-h", 2) || !strncmp(argv[n], "-H", 2)){
+					printf("Sintassi:\n\n");
+					printf("    client -a (indirizzo server) -p (porta del server) [-h].\n\n");
+					exit(EXIT_SUCCESS);
+				}
+		++n;
+	}
+	if (argc == 1){
+		printf("Sintassi:\n\n");
+		printf("    client -a (indirizzo server) -p (porta del server) [-h].\n\n");
+		exit(EXIT_SUCCESS);
+	}
+	return 0;
+}
