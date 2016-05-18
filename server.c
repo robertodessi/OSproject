8af9a23c16327703056385e33fdca3b68a18888b
Roberto <roberto.dessi11@gmail.com>
2016-05-18 02:57:06 +0200

    Gestione segfault, temporizzazione della socket ok ma vedere funzionamente con la select (per ora commentato!)

--- server.c
+++ server.c
@@ -151,6 +151,9 @@ int main(int argc, char *argv[]) {
 
     ret = sigdelset(&mask, SIGPIPE);
     ERROR_HELPER(ret, "Errore nella sigdelset(SIGPIPE)\n\n");
+    
+    ret = sigdelset(&mask, SIGSEGV);
+    ERROR_HELPER(ret, "Errore nella sigdelset(SIGSEGV)\n\n");
 
     ret = pthread_sigmask(SIG_BLOCK, &mask, NULL);
     ERROR_HELPER(ret, "Errore nella pthread_sigmask\n\n");
@@ -210,6 +213,37 @@ int main(int argc, char *argv[]) {
             logMsg("Cannot open socket for incoming connection, retrying...");
             continue;
         }
+        
+        //TIMEOUT on socket client_desc for input and output operations
+        struct timeval timeout;      
+        timeout.tv_sec = TIMEOUT;
+        timeout.tv_usec = 0;
+
+        //ret = setsockopt (client_desc, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout));
+        ret = setsockopt (client_desc, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+        ERROR_HELPER(ret, "Cannot listen on socket");
+        
+        ret = setsockopt (client_desc, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout, sizeof(timeout));
+        ERROR_HELPER(ret, "Cannot listen on socket");
+        
+        /*
+         SO_SNDTIMEO is an option to set a timeout value for output operations. 
+         * It accepts a struct timeval parameter with the number of seconds and microseconds used 
+         * to limit waits for output operations to complete. If a send operation has blocked for 
+         * this much time, it returns with a partial count or with the error EWOULDBLOCK if no data 
+         * were sent. In the current implementation, this timer is restarted each time additional data 
+         * are delivered to the protocol, implying that the limit applies to output portions ranging in
+         *  size from the low-water mark to the high-water mark for output.
+
+         SO_RCVTIMEO is an option to set a timeout value for input operations. It accepts a struct timeval 
+         * parameter with the number of seconds and microseconds used to limit waits for input operations 
+         * to complete. In the current implementation, this timer is restarted each time additional data
+         *  are received by the protocol, and thus the limit is in effect an inactivity timer. If a 
+         * receive operation has been blocked for this much time without receiving additional data, it 
+         * returns with a short count or with the error EWOULDBLOCK if no data were received. The struct 
+         * timeval parameter must represent a positive time interval; otherwise, setsockopt() returns with
+         *  the error EDOM.
+         */
 
         // parse client IP address and port
         char client_ip[INET_ADDRSTRLEN];
